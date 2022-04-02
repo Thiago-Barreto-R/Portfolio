3c9a76cd1da7f5268e39f1790c37589d40b678b1
Thiago Barreto Rodrigues <tbarreto347@gmail.com>
2022-04-02 14:00:46 -0400

    Adicionando o JS no projeto, e já iniciando a parte mobile do header.:sunglasses:

---
+++ js/app.js
@@ -0,0 +1,63 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "show";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+    }
+
+    removeClass() {
+        for (const link of this.navLinks) {
+            link.addEventListener('click', () => {
+                this.navList.classList.toggle(this.activeClass);
+                this.mobileMenu.classList.toggle(this.activeClass);
+            })
+        }
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+            this.removeClass();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".list",
+    ".list li",
+);
+mobileNavbar.init();
+
+const header = document.querySelector('.header-top')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
+
+const navLink = document.querySelectorAll('.item');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
