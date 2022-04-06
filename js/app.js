843a7495dd1dcc701c337967a781f23d91b317bc
Thiago Barreto Rodrigues <tbarreto347@gmail.com>
2022-04-05 22:14:31 -0400

    Fazendo a seção de projetos.

--- js/app.js
+++ js/app.js
@@ -61,3 +61,20 @@ function linkAction() {
     this.classList.add('active');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// slider
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
